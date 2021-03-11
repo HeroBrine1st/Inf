1118d61b66ddd5558b4b354b86496c4b776d1410
HeroBrine1st Erquilenne <herobrine1st@pm.me>
2021-03-11 20:04:53 +0300

    Initial commit

---
+++ src/utils/HideOnScroll.js
@@ -0,0 +1,18 @@
+import Slide from "@material-ui/core/Slide";
+import {useScrollTrigger} from "@material-ui/core";
+import PropTypes from "prop-types";
+import React from "react";
+
+const HideOnScroll = ({children}) => {
+  return (
+    <Slide appear={false} direction="down" in={!useScrollTrigger()}>
+      {children}
+    </Slide>
+  );
+};
+
+HideOnScroll.propTypes = {
+  children: PropTypes.element.isRequired,
+};
+
+export default HideOnScroll
