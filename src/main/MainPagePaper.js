85b7063a8ccec00410d709cbca8303ef418eeed6
HeroBrine1st Erquilenne <herobrine1st@pm.me>
2021-03-22 20:36:08 +0300

    Улучшения дизайна

--- src/main/MainPagePaper.js
+++ src/main/MainPagePaper.js
@@ -2,6 +2,7 @@ import {Button, Paper} from "@material-ui/core";
 import Typography from "@material-ui/core/Typography";
 import {makeStyles} from "@material-ui/core/styles";
 import {Link} from "react-router-dom";
+import {NavigateNextRounded} from "@material-ui/icons";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -34,14 +35,15 @@ function MainPagePaper(props) {
       <Typography variant="h5">{props.children}</Typography>
       <div className={classes.paperContent}>
         <ul>
-          {props.items.map(it => (
-            <li><Typography variant="body2">{it}</Typography></li>
+          {props.items.map((it, index) => (
+            <li key={index}><Typography variant="body2">{it}</Typography></li>
           ))}
         </ul>
       </div>
 
-      <Button variant="outlined" color="primary" className={classes.button} component={Link} to={props.to}>
-        Нажми меня
+      <Button variant="contained" color="primary" className={classes.button} component={Link} to={props.to}
+              endIcon={<NavigateNextRounded/>}>
+        Перейти
       </Button>
     </Paper>
   </div>
