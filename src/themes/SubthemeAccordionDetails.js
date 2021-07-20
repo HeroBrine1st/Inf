bf6e029a2450179db0ae2982ce8c74aae7c1f335
HeroBrine1st Erquilenne <herobrine1st@pm.me>
2021-07-20 21:39:57 +0300

    Плавное преобразование контента при загрузке

--- src/themes/SubthemeAccordionDetails.js
+++ src/themes/SubthemeAccordionDetails.js
@@ -1,6 +1,7 @@
 import DownloadingJson from "../misc/DownloadingJson";
 import {useCallback, useState} from "react";
 import Typography from "@material-ui/core/Typography";
+import {Collapse} from "@material-ui/core";
 
 
 function pluralize(number, a, b, c) {
@@ -12,18 +13,24 @@ function pluralize(number, a, b, c) {
 
 function SubthemeAccordionDetails(props) {
   const [subthemes, setSubthemes] = useState([])
+  const [collapseIn, setCollapseIn] = useState(false)
   return <DownloadingJson
-    onResult={useCallback(it => setSubthemes(it), [])}
+    onResult={useCallback(it => {
+      setSubthemes(it)
+      setTimeout(() => setCollapseIn(true), 0) // Костыль, но работает !
+    }, [])}
     url={`${process.env.REACT_APP_API_ROOT}/themes/${props.id}/subthemes/`}
     nobackdrop linear>
-    <ul>
-      {subthemes.map(it => (
-        <li key={it.id}>
-          <Typography
-            variant="body2">{it.name} - {it["task_count"]} {pluralize(it["task_count"], "задание", "задания", "заданий")}</Typography>
-        </li>
-      ))}
-    </ul>
+    <Collapse in={collapseIn}>
+      <ul>
+        {subthemes.map(it => (
+          <li key={it.id}>
+            <Typography
+              variant="body2">{it.name} - {it["task_count"]} {pluralize(it["task_count"], "задание", "задания", "заданий")}</Typography>
+          </li>
+        ))}
+      </ul>
+    </Collapse>
   </DownloadingJson>
 }
 
