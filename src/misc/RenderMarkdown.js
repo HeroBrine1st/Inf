b6f9c2b604f9f3a211f79d2a0728c9d09658f463
HeroBrine1st Erquilenne <herobrine1st@pm.me>
2021-07-22 16:27:24 +0300

    Added remark-math rehype-katex

--- src/misc/RenderMarkdown.js
+++ src/misc/RenderMarkdown.js
@@ -1,6 +1,9 @@
 import ReactMarkdown from "react-markdown";
 import {makeStyles} from "@material-ui/core/styles";
 import PropTypes from "prop-types";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
+import 'katex/dist/katex.min.css'
 
 const useStyles = makeStyles(() => ({
   markdown: {}, // Без стиля компонент не создаст свой <div/>
@@ -8,7 +11,10 @@ const useStyles = makeStyles(() => ({
 
 function RenderMarkdown(props) {
   const classes = useStyles()
-  return <ReactMarkdown className={classes.markdown}>
+  return <ReactMarkdown
+    remarkPlugins={[remarkMath]}
+    rehypePlugins={[rehypeKatex]}
+    className={classes.markdown}>
     {props.children}
   </ReactMarkdown>
 }
